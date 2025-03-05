82aff8923400d870675c96aeb2e3de94a636ce93
shubham soni <ssoni190@gmail.com>
2025-03-05 18:02:51 +0530

    #fixes AWS deploy

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ import { sendWarningsToDoctors } from './cron_jobs/cronFunctions.js';
   config();
   app.use(express.json());
   app.use(cors("*"));
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
   // const corsOptions = {
   //   origin: ['http://localhost:3000'], // Add your frontend's URLs
