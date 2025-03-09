7647bc63ddb76fa9b8a5a72b8306665200f76217
shubham soni <ssoni190@gmail.com>
2025-03-09 21:19:10 +0530

    #implementation of SSL

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
 import { setupDatabase } from './config/db.js';
-import express, { json } from 'express';
+import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 import routes from './v1/profile/routes/routes.js'
-
 import { runCronJobs } from './cron_jobs/schedulers.js';
 import { createS3Bucket, createDefaultFolder } from './utils/upload.js';
 import { sendWarningsToDoctors } from './cron_jobs/cronFunctions.js';
+config();
+
 (async () => {
   await setupDatabase();
 
   const app = express();
-  config();
   app.use(express.json());
   app.use(cors("*"));
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
   const corsOptions = {
-    origin: ['http://localhost:3000', "https://dental-insight-studio-react-fork-pxmn.vercel.app"], // Add your frontend's URLs
+    origin: ['http://localhost:3000', "https://dental-insight-studio-react-fork-pxmn.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'], // Explicitly allow necessary headers
-    credentials: true,  // Allows sending cookies & Authorization headers
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
+    credentials: true,
   };
-
+  
   app.use(cors(corsOptions));
   app.options('*', cors(corsOptions));
 
-  // Import & Define API versions
   app.use('/api/v1', routes);
-  // await createS3Bucket();
-  // await createDefaultFolder();
-  // await sendWarningsToDoctors()
   app.use('/', (req, res) => {
-    res.send("Hey, I'm online now!!");
+    res.send("Hey, I'm online now with HTTPS!!");
+  });
+  
+  runCronJobs();
+
+  const PORT = process.env.PORT || 4000;
+
+  // Load SSL certificate
+  const sslOptions = {
+    key: fs.readFileSync('/etc/ssl/private/selfsigned.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt'),
+  };
+
+  // Start HTTPS Server on the specified port
+  https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`Server is running on HTTPS port ${PORT}`);
   });
 
-  runCronJobs()
-  // Start the server
-  const port = process.env.PORT || 4000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
 })();
